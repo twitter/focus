e990e39e6e383edc3ea2f909ad106d4e8f6247c0
Wilhelm Bierbaum <wilhelm@twitter.com>
2021-08-04 15:23:10 -0700

    Tool class

--- focus/commands/src/cli/tool.rs
+++ focus/commands/src/cli/tool.rs
@@ -19,23 +19,24 @@ impl Tool {
         Ok((Stdio::from(file), path))
     }
 
-    pub fn invoke(&self, args: Vec<OsString>, dir: Option<&Path>, stdin_file: Option<Stdio>, sandbox: &Sandbox) -> Result<InvocationResult> {
+    pub fn invoke(&self, args: Option<&Vec<OsString>>, dir: Option<&Path>, stdin_file: Option<Stdio>, sandbox: &Sandbox) -> Result<InvocationResult> {
         let (stdout_stdio, stdout_path) = 
             Self::captor(&self.name, "stdout", &sandbox)?;
         let (stderr_stdio, stderr_path) = 
             Self::captor(&self.name, "stderr", &sandbox)?;
-
+        let args = args.unwrap_or(&Vec::<OsString>::new()).to_owned();
         let exit_status = Command::new(&self.executable)
             .stdin(stdin_file.unwrap_or(Stdio::null()))
             .stdout(stdout_stdio)
             .stderr(stderr_stdio)
+            .args(&args)
             .spawn().with_context(|| { format!("spawning {}", self.name)})?
             .wait().with_context(|| { format!("waiting on {}", self.name)})?;
         
         Ok(InvocationResult{
             name: self.name.clone(),
             binary: self.executable.to_owned(),
-            args: vec![],
+            args: args,
             exit_status,
             stdout_path,
             stderr_path
@@ -127,13 +128,13 @@ mod tests {
     use std::io::{BufReader, SeekFrom, Write, prelude::*};
     use std::ffi::OsStr; 
 
-    fn init() {
+    fn init_logging() {
         let _ = env_logger::builder().is_test(true).try_init();
     }
 
     #[test]
     fn concatenating() -> Result<()> {
-        init();
+        init_logging();
         let sandbox = Sandbox::new(false)?;
         let (mut file, path) = sandbox.create_file(None, Some("txt"))?;
         let original_contents = "hello there\n".as_bytes();
@@ -141,9 +142,26 @@ mod tests {
         file.seek(SeekFrom::Start(0))?;
         file.sync_all()?;
         let cat = Tool::new("cat", OsStr::new("cat"))?;
-        let args = Vec::<OsString>::new();
         let reopened_file = File::open(path).context("reopening file")?;
-        let invocation = cat.invoke(args, None, Some(Stdio::from(reopened_file)), &sandbox)?;
+        let invocation = cat.invoke(None, None, Some(Stdio::from(reopened_file)), &sandbox)?;
+        assert!(invocation.or_display_logs().is_ok());
+        let mut reader =
+             BufReader::new(File::open(invocation.stdout_path())?);
+        let mut contents = Vec::<u8>::new();
+        reader.read_to_end(&mut contents)?;
+        assert_eq!(contents, original_contents);
+        
+        Ok(())
+    }
+
+    #[test]
+    fn arg_handling() -> Result<()> {
+        init_logging();
+        let sandbox = Sandbox::new(false)?;
+        let original_contents = "fee fie foe fum".as_bytes();
+        let echo = Tool::new("echo", OsStr::new("echo"))?;
+        let args = vec![OsString::from("-n"), OsString::from("fee"), OsString::from("fie"), OsString::from("foe"), OsString::from("fum")];
+        let invocation = echo.invoke(Some(&args), None, None, &sandbox)?;
         assert!(invocation.or_display_logs().is_ok());
         let mut reader =
              BufReader::new(File::open(invocation.stdout_path())?);
