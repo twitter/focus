b54ce657bea411a6b0982672f7f8f79b2e3bd6eb
Wilhelm Bierbaum <wilhelm@twitter.com>
2021-08-26 16:42:36 -0700

    Describe ad-hoc layers in selected-layers command

--- focus/commands/src/cli/subcommands/selected_layers.rs
+++ focus/commands/src/cli/subcommands/selected_layers.rs
@@ -6,6 +6,7 @@ use crate::model::LayerSets;
 
 pub fn run(repo: &Path) -> Result<()> {
     let sets = LayerSets::new(&repo);
+
     if let Some(selected) = sets.selected_layers().context("loading selected layers")? {
         // TODO: Extract printing and re-use
         if selected.layers().is_empty() {
@@ -18,6 +19,14 @@ pub fn run(repo: &Path) -> Result<()> {
     } else {
         eprintln!("No layers are selected, and no stack exists");
     }
+
+    if let Ok(Some(adhoc_layers)) = sets.adhoc_layers() {
+        eprintln!("Note: An ad-hoc layer set exists, containing these layers:");
+        for layer in adhoc_layers.layers() {
+            eprintln!("[ad-hoc]: {}", layer);
+        }
+    }
+
 
     Ok(())
 }
