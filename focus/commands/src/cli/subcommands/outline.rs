9dcc4d6586707516af01f11104e33de803316bea
Wilhelm Bierbaum <wilhelm@twitter.com>
2021-09-21 09:46:56 -0700

    Fix outline command skeleton import issue

--- focus/commands/src/cli/subcommands/outline.rs
+++ focus/commands/src/cli/subcommands/outline.rs
@@ -1,5 +1,5 @@
 use anyhow::{bail, Context, Result};
-
+use std::path::Path;
 
 pub fn run(source: &Path, mount_point: &Path, coordinates: Vec<String>) -> Result<()> {
     let source = std::fs::canonicalize(source).context("canonicalizing source path")?;
