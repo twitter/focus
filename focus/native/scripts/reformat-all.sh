df5052cea012a767b2cccf1e318b8f7e2de444b3
Wilhelm Bierbaum <wilhelm@twitter.com>
2021-12-14 13:58:19 -0800

    Remove unused native code

--- focus/native/scripts/reformat-all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-fd -e cpp,h -e h -E third_party -x clang-format -i {}
--- focus/native/spyfs/annotations.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
-  TypeName(TypeName&) = delete;            \
-  void operator=(TypeName) = delete;
-
-#define DISALLOW_MOVE(TypeName)  \
-  TypeName(TypeName&&) = delete; \
-  TypeName& operator=(TypeName&&) = delete;
