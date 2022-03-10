633c0f79eb81d94e3dabbc7e14f69ba11a396cfe
Waleed Khan <me@waleedkhan.name>
2022-03-10 12:06:25 -0800

    [focus] Add `Label` type
    Summary:
    This type lets us parse the components of a Bazel target up-front and use the data later. Currently, Pants labels are similar, but we've just left them as `String`s.
    
    It might be a little over-engineered; we can simplify it according to reviewer suggestions, if any.
    
    Test Plan:
    Added unit tests for parsing.
    
    ```
    $ cargo test
    ```
    
    Differential Revision: https://phabricator.twitter.biz/D850108

--- focus/internals/src/lib/coordinate.rs
+++ focus/internals/src/lib/coordinate.rs
@@ -1,4 +1,6 @@
 use anyhow::Result;
+use std::fmt::Debug;
+use std::str::FromStr;
 use std::{collections::HashSet, convert::TryFrom, fmt::Display};
 
 use thiserror::Error;
@@ -67,10 +69,15 @@ impl TryFrom<&[String]> for CoordinateSet {
     }
 }
 
-#[derive(Debug, Eq, PartialEq, Hash, Clone)]
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Clone)]
 pub enum Coordinate {
-    Bazel(String),
+    /// A Bazel package like `//foo/bar:baz`.
+    Bazel(Label),
+
+    /// A specific directory within the repository.
     Directory(String),
+
+    /// A Pants package like `foo/bar:baz`.
     Pants(String),
 }
 
@@ -91,6 +98,9 @@ pub enum CoordinateError {
 
     #[error("Failed to tokenize input")]
     TokenizationError,
+
+    #[error("Failed to parse label")]
+    LabelError(#[from] LabelParseError),
 }
 
 impl TryFrom<&str> for Coordinate {
@@ -101,7 +111,8 @@ impl TryFrom<&str> for Coordinate {
             Some((prefix, rest)) => {
                 let rest = rest.to_owned();
                 if prefix.eq_ignore_ascii_case("bazel") {
-                    Ok(Coordinate::Bazel(rest))
+                    let label: Label = rest.parse()?;
+                    Ok(Coordinate::Bazel(label))
                 } else if prefix.eq_ignore_ascii_case("directory") {
                     Ok(Coordinate::Directory(rest))
                 } else if prefix.eq_ignore_ascii_case("pants") {
@@ -115,20 +126,151 @@ impl TryFrom<&str> for Coordinate {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum TargetName {
+    Name(String),
+    Ellipsis,
+}
+
+/// A Bazel label referring to a specific target.
+///
+/// See <https://docs.bazel.build/versions/main/build-ref.html#labels>. Note
+/// that a label does *not* refer to a package.
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Label {
+    /// For a label like `@foo//bar:baz`, this would be `@foo`. If there is no
+    /// `@`-component, then this is `None`.
+    pub(crate) external_repository: Option<String>,
+
+    /// The directory components of the path after `//`.
+    ///
+    /// The leading `//` is optional and inferred if present (i.e. a label
+    /// `foo/bar` is assumed to be the same as `//foo/bar`, and not instead
+    /// relative to the current directory.)
+    pub(crate) path_components: Vec<String>,
+
+    /// If no explicit target name is given, it is inferred from the last path
+    /// component. For a label like `//foo/bar:bar` or `//foo/bar`, this would
+    /// be `bar`.
+    pub(crate) target_name: TargetName,
+}
+
+impl Display for Label {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "//{}",
+            // Note that `path_components` may be empty, which is fine.
+            self.path_components.join("/")
+        )?;
+
+        match &self.target_name {
+            TargetName::Name(name) => {
+                write!(f, ":{}", name)?;
+            }
+            TargetName::Ellipsis => {
+                write!(f, "/...")?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl Debug for Label {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, r#"Label("{}")"#, self)
+    }
+}
+
+/// TODO: improve error messaging here
+#[derive(Error, Debug, PartialEq)]
+pub enum LabelParseError {
+    #[error("No target name")]
+    NoTargetName,
+}
+
+impl FromStr for Label {
+    type Err = LabelParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (external_package, label) = match s.split_once("//") {
+            None => (None, s),
+            Some(("", label)) => (None, label),
+            Some((external_package, label)) => (Some(external_package.to_string()), label),
+        };
+
+        let mut path_components: Vec<String> = label.split('/').map(|s| s.to_string()).collect();
+        let target_name = match path_components.pop() {
+            Some(target_name) => target_name,
+            None => return Err(LabelParseError::NoTargetName),
+        };
+
+        if target_name == "..." {
+            Ok(Self {
+                external_repository: external_package,
+                path_components,
+                target_name: TargetName::Ellipsis,
+            })
+        } else {
+            let (last_component, target_name) = match target_name.split_once(':') {
+                Some((last_component, target_name)) => (last_component, target_name),
+                None => (target_name.as_str(), target_name.as_str()),
+            };
+
+            path_components.push(last_component.to_string());
+            Ok(Self {
+                external_repository: external_package,
+                path_components,
+                target_name: TargetName::Name(target_name.to_string()),
+            })
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::convert::TryFrom;
 
     use anyhow::Result;
 
-    use crate::coordinate::{Coordinate, CoordinateError, CoordinateSet};
+    use super::*;
 
     #[test]
     pub fn coordinate_parsing() -> Result<()> {
         assert_eq!(
             Coordinate::try_from("bazel://a:b")?,
-            Coordinate::Bazel("//a:b".to_owned())
+            Coordinate::Bazel(Label {
+                external_repository: None,
+                path_components: vec!["a".to_string()],
+                target_name: TargetName::Name("b".to_string()),
+            })
         );
+        assert_eq!(
+            Coordinate::try_from("bazel://foo"),
+            Ok(Coordinate::Bazel(Label {
+                external_repository: None,
+                path_components: vec!["foo".to_string()],
+                target_name: TargetName::Name("foo".to_string())
+            }))
+        );
+        assert_eq!(
+            Coordinate::try_from("bazel://foo/bar/..."),
+            Ok(Coordinate::Bazel(Label {
+                external_repository: None,
+                path_components: vec!["foo".to_string(), "bar".to_string()],
+                target_name: TargetName::Ellipsis,
+            }))
+        );
+        assert_eq!(
+            Coordinate::try_from("bazel:@foo//bar:qux"),
+            Ok(Coordinate::Bazel(Label {
+                external_repository: Some("@foo".to_string()),
+                path_components: vec!["bar".to_string()],
+                target_name: TargetName::Name("qux".to_string()),
+            }))
+        );
+
         assert_eq!(
             Coordinate::try_from("bogus:whatever").unwrap_err(),
             CoordinateError::UnsupportedScheme("bogus".to_owned())
