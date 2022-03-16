fa0feb85ff487783b6a2d7f455bb36d572793cac
Wilhelm Bierbaum <wilhelm@twitter.com>
2022-03-16 11:26:26 -0700

    Improve diagnostics when opening repos
    Differential Revision: https://phabricator.twitter.biz/D853793

--- focus/internals/src/lib/model/repo.rs
+++ focus/internals/src/lib/model/repo.rs
@@ -401,7 +401,9 @@ pub struct Repo {
 
 impl Repo {
     pub fn open(path: &Path, app: Arc<App>) -> Result<Self> {
-        let repo = git2::Repository::open(&path).context("Failed to open tree")?;
+        let repo = git2::Repository::open(&path)
+            .with_context(|| format!("Opening repo {}", path.display()))
+            .context("Failed to open repo")?;
         if repo.is_bare() {
             bail!("Bare repos are not supported");
         }
