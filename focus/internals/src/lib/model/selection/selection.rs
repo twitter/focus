d2a3a8ce0a826ba9730db0997d2f1e49a53b8be1
Wilhelm Bierbaum <wilhelm@twitter.com>
2022-05-10 16:28:26 -0700

    Move everything selection related into the 'selection' module

--- focus/internals/src/lib/model/selection/selection.rs
+++ focus/internals/src/lib/model/selection/selection.rs
@@ -1,8 +1,10 @@
-use anyhow::{Context, Result};
+use anyhow::{Context, Result, bail};
 use std::{
     collections::{BTreeSet, HashSet},
     fmt::Display,
+    path::{PathBuf, Path},
 };
+use tracing::{debug, error, warn};
 
 use super::*;
 
@@ -106,5 +108,189 @@ impl Display for Selection {
         }
 
         Ok(())
+    }
+}
+
+pub(crate) struct SelectionOperationProcessor<'processor> {
+    pub selection: &'processor mut Selection,
+    pub projects: &'processor Projects,
+}
+
+impl<'processor> SelectionOperationProcessor<'processor> {
+    pub fn process(&mut self, operations: &Vec<Operation>) -> Result<OperationResult> {
+        let mut result: OperationResult = Default::default();
+
+        for operation in operations {
+            debug!(?operation, "Processing operation");
+            match (&operation.action, &operation.underlying) {
+                (OperationAction::Add, Underlying::Target(target)) => {
+                    if self.selection.targets.insert(target.clone()) {
+                        result.added.insert(operation.underlying.clone());
+                        debug!(?target, "Target added to selection")
+                    } else {
+                        result.ignored.insert(operation.underlying.clone());
+                        debug!(?target, "Target already in selection")
+                    }
+                }
+                (OperationAction::Add, Underlying::Project(name)) => {
+                    match self.projects.underlying.get(name.as_str()) {
+                        Some(project) => {
+                            if self.selection.projects.insert(project.clone()) {
+                                result.added.insert(operation.underlying.clone());
+                                debug!(?project, "Project added to selection");
+                            } else {
+                                result.ignored.insert(operation.underlying.clone());
+                                debug!(?project, "Project already in selection");
+                            }
+                        }
+                        None => {
+                            warn!(%name, "Project to be added was not found");
+                            result.absent.insert(operation.underlying.clone());
+                        }
+                    }
+                }
+                (OperationAction::Remove, Underlying::Target(target)) => {
+                    if self.selection.targets.remove(target) {
+                        debug!(?target, "Target removed from selection");
+                        result.removed.insert(operation.underlying.clone());
+                    } else {
+                        warn!(?target, "Target to be removed was not in selection");
+                        result.ignored.insert(operation.underlying.clone());
+                    }
+                }
+                (OperationAction::Remove, Underlying::Project(name)) => {
+                    match self.projects.underlying.get(name) {
+                        Some(project) => {
+                            if self.selection.projects.remove(project) {
+                                debug!(?project, "Project removed from selection");
+                                result.removed.insert(operation.underlying.clone());
+                            } else {
+                                warn!(%name, "Project to be removed was not in selection");
+                                result.ignored.insert(operation.underlying.clone());
+                            }
+                        }
+                        None => {
+                            error!(%name, "Project to be removed is not a defined project");
+                            result.absent.insert(operation.underlying.clone());
+                        }
+                    }
+                }
+            }
+        }
+
+        Ok(result)
+    }
+}
+
+pub struct Selections {
+    selection_path: PathBuf,
+    pub optional_projects: Projects,
+    pub mandatory_projects: Projects,
+    selection: Selection,
+}
+
+impl Selections {
+    pub fn from_repo(repo: &Repo) -> Result<Self> {
+        let working_tree = repo
+            .working_tree()
+            .ok_or_else(|| anyhow::anyhow!("The repo must have a working tree"))?;
+
+        let paths = DataPaths::from_working_tree(working_tree)?;
+        let optional_projects = Projects::new(
+            ProjectSets::new(&paths.project_dir).context("Loading optional projects")?,
+        )?;
+        let mandatory_projects = Projects::new(
+            ProjectSets::new(&paths.focus_dir).context("Loading mandatory projects")?,
+        )?;
+
+        Self::new(&paths.selection_file, optional_projects, mandatory_projects)
+    }
+
+    fn new(
+        selection_path: impl AsRef<Path>,
+        optional_projects: Projects,
+        mandatory_projects: Projects,
+    ) -> Result<Self> {
+        let mut instance = Self {
+            selection_path: selection_path.as_ref().to_owned(),
+            optional_projects,
+            mandatory_projects,
+            selection: Default::default(),
+        };
+        instance.reload()?;
+        Ok(instance)
+    }
+
+    /// Load a selection from the given `path` using project definitions from `projects`.
+    fn load(path: impl AsRef<Path>, projects: &Projects) -> Result<Selection> {
+        let persisted_selection = load_model(path).context("Loading persisted selection")?;
+        Selection::from_persisted_selection(persisted_selection, projects)
+    }
+
+    /// Load the selection from disk.
+    pub fn reload(&mut self) -> Result<()> {
+        let selection: Selection = Self::load(&self.selection_path, &self.optional_projects)?;
+        debug!(?selection, path = ?self.selection_path, "Reloaded selection");
+        self.selection = selection;
+        Ok(())
+    }
+
+    /// Save the current selection to the configured `selection_path`.
+    pub fn save(&self) -> Result<()> {
+        let selection = self.selection.clone();
+        let persisted_selection = PersistedSelection::from(&selection);
+        store_model(&self.selection_path, &persisted_selection)?;
+        debug!(?persisted_selection, path = ?self.selection_path, "Saved selection");
+        Ok(())
+    }
+
+    /// Returns a Selection combining both user-selected and mandatory projects and targets.
+    pub fn computed_selection(&self) -> Result<Selection> {
+        let mut selection = self.selection.clone();
+        debug!(selected = ?selection, "User-selected projects");
+        let mandatory_projects = self
+            .mandatory_projects
+            .underlying
+            .values()
+            .cloned()
+            .collect::<Vec<Project>>();
+        debug!(mandatory = ?mandatory_projects, "Mandatory projects");
+        selection.projects.extend(mandatory_projects);
+        Ok(selection)
+    }
+
+    pub fn mutate(
+        &mut self,
+        action: OperationAction,
+        projects_and_targets: &Vec<String>,
+    ) -> Result<bool> {
+        let operations = projects_and_targets
+            .iter()
+            .map(|value| Operation::new(action, value.clone()))
+            .collect::<Vec<Operation>>();
+        let result = self.process(&operations)?;
+        if !result.is_success() {
+            bail!("Failed to update the selection");
+        }
+        Ok(result.changed())
+    }
+
+    pub fn process(&mut self, operations: &Vec<Operation>) -> Result<OperationResult> {
+        let mut selection = self.selection.clone();
+        let mut processor = SelectionOperationProcessor {
+            selection: &mut selection,
+            projects: &self.optional_projects,
+        };
+        match processor.process(operations) {
+            Ok(result) => {
+                if result.is_success() {
+                    self.selection = selection;
+                } else {
+                    error!("The selection will not be updated because an error occured while applying the requested changes");
+                }
+                Ok(result)
+            }
+            Err(e) => Err(e),
+        }
     }
 }
