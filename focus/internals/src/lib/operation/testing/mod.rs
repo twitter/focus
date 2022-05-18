15177f3534c957ee2e7cb9a4b9769d9fa78af364
Waleed Khan <me@waleedkhan.name>
2022-05-18 10:23:19 -0700

    [focus] Add `--fetch-index` option to `focus sync`
    Summary: For debugging, to make sure I don't accidentally load a remote index.
    
    Reviewers: #focus
    
    JIRA Issues: PFD-201
    
    Differential Revision: https://phabricator.twitter.biz/D895970

--- focus/internals/src/lib/operation/testing/mod.rs
+++ focus/internals/src/lib/operation/testing/mod.rs
@@ -279,7 +279,7 @@ pub(in crate::operation) mod integration {
 
         #[allow(dead_code)]
         pub fn perform_sync(&self) -> Result<bool> {
-            operation::sync::run(&self.sparse_repo_path, self.app.clone())
+            operation::sync::run(&self.sparse_repo_path, self.app.clone(), false)
         }
 
         #[allow(dead_code)]
