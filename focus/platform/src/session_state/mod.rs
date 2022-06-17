12fe1508d4ebf27ed739a403c3add06c4e3c00ce
Wilhelm Bierbaum <wilhelm@twitter.com>
2022-06-17 12:57:39 -0700

    Add a focus-platform crate for platform-specific code
    Summary: The crate currently provides a facility to understand whether the machine is idle
    
    Reviewers: dbernadett, O57517 focus:/
    
    Differential Revision: https://phabricator.twitter.biz/D917713

---
+++ focus/platform/src/session_state/mod.rs
@@ -0,0 +1,25 @@
+/// An enumeration representing whether the computer is being actively used.
+#[allow(dead_code)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum SessionStatus {
+    /// The computer is being actively used.
+    Active,
+
+    /// The computer has not been interacted with for the given duration.
+    Idle,
+
+    /// It is not possible to determine whether the computer is being interacted with.
+    Unknown,
+}
+
+#[cfg(target_os = "macos")]
+pub mod macos;
+
+#[cfg(target_os = "macos")]
+pub use macos::has_session_been_idle_for;
+
+/// Default implementation
+#[cfg(not(target_os = "macos"))]
+fn has_session_been_idle_for(_at_least: Duration) -> SessionStatus {
+    SessionStatus::Unknown
+}
