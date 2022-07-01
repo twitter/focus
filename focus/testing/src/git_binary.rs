f2e4881ff413a7172e81b0135380df9761a3f953
Waleed Khan <me@waleedkhan.name>
2022-06-30 17:32:15 -0700

    [focus] Fix build with Twitter Git
    Summary: I mistakenly wasn't testing with Twitter Git, only open-source Git, so I missed these failures.
    
    Test Plan:
    ```
    $ cargo test --workspace
    ```
    
    Reviewers: #focus, kpaul, O57517 focus:/
    
    JIRA Issues: PFD-238
    
    Differential Revision: https://phabricator.twitter.biz/D926286

--- focus/testing/src/git_binary.rs
+++ focus/testing/src/git_binary.rs
@@ -6,27 +6,61 @@ use std::ffi::OsString;
 use std::os::unix::ffi::OsStringExt;
 use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
+use std::sync::Arc;
+use tempfile::TempDir;
 
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Clone, Debug)]
 pub struct GitBinary {
+    pub home_temp_dir: Option<Arc<TempDir>>,
     pub git_binary_path: PathBuf,
     pub git_exec_path: PathBuf,
 }
 
+impl PartialEq for GitBinary {
+    fn eq(&self, other: &Self) -> bool {
+        let Self {
+            home_temp_dir: _,
+            git_binary_path,
+            git_exec_path,
+        } = self;
+        let Self {
+            home_temp_dir: _,
+            git_binary_path: other_git_binary_path,
+            git_exec_path: other_git_exec_path,
+        } = other;
+
+        git_binary_path == other_git_binary_path && git_exec_path == other_git_exec_path
+    }
+}
+
+impl Eq for GitBinary {}
+
+fn home_temp_dir() -> anyhow::Result<Option<Arc<TempDir>>> {
+    if cfg!(test) {
+        Ok(Some(Arc::new(tempfile::tempdir()?)))
+    } else {
+        Ok(None)
+    }
+}
+
 impl GitBinary {
     pub fn from_env() -> anyhow::Result<Self> {
+        let home_temp_dir = home_temp_dir()?;
         let git_binary_path =
             which::which("git").context("Resolving absolute path for git binary")?;
         let git_exec_path = git_exec_path(&git_binary_path)?;
         Ok(Self {
+            home_temp_dir,
             git_binary_path,
             git_exec_path,
         })
     }
 
     pub fn from_binary_path(git_binary_path: PathBuf) -> anyhow::Result<Self> {
+        let home_temp_dir = home_temp_dir()?;
         let git_exec_path = git_exec_path(&git_binary_path)?;
         Ok(Self {
+            home_temp_dir,
             git_binary_path,
             git_exec_path,
         })
@@ -35,7 +69,12 @@ impl GitBinary {
     pub fn command(&self) -> Command {
         let mut command = Command::new(&self.git_binary_path);
         command.env_clear();
+        command.env("HOME", &self.git_exec_path);
         command.env("GIT_EXEC_PATH", &self.git_exec_path);
+        command.env("GIT_AUTHOR_NAME", "Focus Testing");
+        command.env("GIT_AUTHOR_EMAIL", "focus@example.com");
+        command.env("GIT_COMMITTER_NAME", "Focus Testing");
+        command.env("GIT_COMMITTER_EMAIL", "focus@example.com");
         command
     }
 }
