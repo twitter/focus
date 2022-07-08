22f10312d818a148c92bf00c804686c65cd85402
Waleed Khan <me@waleedkhan.name>
2022-07-08 15:05:53 -0700

    [focus] Parallelize `calc_invalidation_rate` script
    JIRA Issues: PFD-249
    
    Differential Revision: https://phabricator.twitter.biz/D931542

--- focus/commands/examples/calc_invalidation_rate.rs
+++ focus/commands/examples/calc_invalidation_rate.rs
@@ -6,8 +6,12 @@
 
 use std::{
     collections::{HashMap, HashSet},
+    ops::Deref,
     path::PathBuf,
-    sync::Arc,
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc, Mutex,
+    },
 };
 
 use clap::Parser;
@@ -17,6 +21,7 @@ use focus_internals::{
     target::{Target, TargetSet},
 };
 use focus_util::{app::App, git_helper};
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 #[derive(Parser, Debug)]
 struct Opts {
@@ -24,6 +29,52 @@ struct Opts {
 
     /// The number of commits backward from the `HEAD` commit to sample.
     num_commits: usize,
+}
+
+struct RepoPool {
+    repo_path: PathBuf,
+    repos: Arc<Mutex<Vec<git2::Repository>>>,
+}
+
+struct RepoHandle {
+    repo: Option<git2::Repository>,
+    repos: Arc<Mutex<Vec<git2::Repository>>>,
+}
+
+impl Deref for RepoHandle {
+    type Target = git2::Repository;
+
+    fn deref(&self) -> &Self::Target {
+        self.repo.as_ref().unwrap()
+    }
+}
+
+impl Drop for RepoHandle {
+    fn drop(&mut self) {
+        let mut repos = self.repos.lock().unwrap();
+        repos.push(self.repo.take().unwrap());
+    }
+}
+
+impl RepoPool {
+    fn new(repo_path: PathBuf) -> Self {
+        Self {
+            repo_path,
+            repos: Default::default(),
+        }
+    }
+
+    fn get(&self) -> anyhow::Result<RepoHandle> {
+        let mut repos = self.repos.lock().unwrap();
+        let repo = match repos.pop() {
+            Some(repo) => repo,
+            None => git2::Repository::open(&self.repo_path)?,
+        };
+        Ok(RepoHandle {
+            repo: Some(repo),
+            repos: Arc::clone(&self.repos),
+        })
+    }
 }
 
 fn average(values: impl IntoIterator<Item = f64>) -> f64 {
@@ -43,7 +94,7 @@ fn main() -> anyhow::Result<()> {
     } = Opts::parse();
 
     let app = Arc::new(App::new(false, None, None, None)?);
-    let repo = Repo::open(&sparse_repo_path, app.clone())?;
+    let repo = Repo::open(&sparse_repo_path, app)?;
     let selections = repo.selection_manager()?;
     let all_targets = {
         let mut targets = TargetSet::try_from(&selections.project_catalog().mandatory_projects)?;
@@ -53,33 +104,67 @@ fn main() -> anyhow::Result<()> {
         targets
     };
 
+    let repo = git2::Repository::open(sparse_repo_path.clone())?;
+    let commits = {
+        eprintln!("Loading {num_commits} commits...");
+        let mut commit = git_helper::get_head_commit(&repo)?;
+        let mut result = Vec::new();
+        for i in 0..num_commits {
+            result.push(commit.id());
+            commit = match commit.parents().next() {
+                Some(commit) => commit,
+                None => {
+                    eprintln!("Stopped after {i} commits.");
+                    break;
+                }
+            };
+        }
+        result
+    };
+
+    let repo_pool = RepoPool::new(sparse_repo_path);
+    let num_finished_commits = AtomicUsize::new(0);
+    let hashes: HashMap<(git2::Oid, &Target), ContentHash> = commits
+        .par_iter()
+        .flat_map(|commit_oid| {
+            let repo = repo_pool.get().unwrap();
+            let commit = repo.find_commit(*commit_oid).unwrap();
+            eprintln!(
+                "Hashing {}/{num_commits}: {:?}",
+                num_finished_commits.fetch_add(1, Ordering::SeqCst),
+                commit.summary().unwrap()
+            );
+            let tree = commit.tree().unwrap();
+            let hash_context = HashContext {
+                repo: &repo,
+                head_tree: &tree,
+                caches: Default::default(),
+            };
+            all_targets
+                .iter()
+                .map(|target| {
+                    let dep_key = DependencyKey::from(target.clone());
+                    let hash =
+                        content_hash_dependency_key(&hash_context, &dep_key, &mut HashSet::new())
+                            .unwrap();
+                    ((*commit_oid, target), hash)
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect();
     #[derive(Clone, Debug)]
     struct HashChangeInfo {
         current_hash: ContentHash,
         commits_since_last_hash_change: usize,
     }
-    let mut target_stats: HashMap<Target, Vec<HashChangeInfo>> = all_targets
-        .into_iter()
+    let mut target_stats: HashMap<&Target, Vec<HashChangeInfo>> = all_targets
+        .iter()
         .map(|target| (target, Vec::new()))
         .collect();
 
-    let repo = git2::Repository::open(sparse_repo_path)?;
-    let mut commit = git_helper::get_head_commit(&repo)?;
-    for i in 0..num_commits {
-        eprintln!(
-            "Hashing {i}/{num_commits} {:?}",
-            commit.summary().unwrap_or_default()
-        );
-        let tree = commit.tree()?;
-        let hash_context = HashContext {
-            repo: &repo,
-            head_tree: &tree,
-            caches: Default::default(),
-        };
-
+    for commit_oid in &commits {
         for (target, hash_change_infos) in target_stats.iter_mut() {
-            let dep_key = DependencyKey::from(target.clone());
-            let hash = content_hash_dependency_key(&hash_context, &dep_key, &mut HashSet::new())?;
+            let hash = hashes[&(*commit_oid, *target)].clone();
             match hash_change_infos.last_mut() {
                 Some(hash_change_info) if hash_change_info.current_hash == hash => {
                     hash_change_info.commits_since_last_hash_change += 1;
@@ -92,7 +177,6 @@ fn main() -> anyhow::Result<()> {
                 }
             }
         }
-        commit = commit.parent(0)?;
     }
 
     let mut averages: Vec<_> = target_stats
