76574f2f1fec3d1689c48057ae10fab52a694774
Waleed Khan <me@waleedkhan.name>
2022-08-01 14:56:19 -0700

    [focus] Add stubs for `tool-insights-client`
    This is Twitter-only, so expose a stub instead of the real dependency.
    
    JIRA Issues: OSS-6294
    
    Differential Revision: https://phabricator.twitter.biz/D947166

---
+++ focus/util/src/stubs.rs
@@ -0,0 +1,62 @@
+//! Stubs for Twitter-internal infrastructure.
+
+pub mod tool_insights_client {
+    use std::collections::HashMap;
+    use std::sync::{Arc, Mutex, MutexGuard};
+    use std::time::SystemTime;
+
+    use anyhow::Result;
+
+    #[derive(Clone)]
+    pub struct Client {
+        underlying: Arc<Mutex<Underlying>>,
+    }
+
+    impl Client {
+        pub fn new(_tool_name: String, _tool_version: String, _start_time: SystemTime) -> Self {
+            Client {
+                underlying: Arc::new(Mutex::new(Underlying)),
+            }
+        }
+
+        pub fn get_context(&self) -> ClientGuard {
+            ClientGuard
+        }
+
+        pub fn get_inner(&self) -> MutexGuard<'_, Underlying> {
+            self.underlying.lock().unwrap()
+        }
+    }
+
+    pub struct ClientGuard;
+
+    impl ClientGuard {
+        pub fn set_tool_feature_name(&mut self, _tool_feature_name: impl Into<String>) {}
+        pub fn set_custom_map(&mut self, _custom_map: HashMap<String, String>) {}
+        pub fn set_exit_code(&mut self, _exit_code: i32) {}
+        pub fn add_to_custom_map(&mut self, _key: impl Into<String>, _value: impl Into<String>) {}
+    }
+
+    pub struct Underlying;
+
+    impl Underlying {
+        pub fn write_message(&self) -> Result<()> {
+            Ok(())
+        }
+
+        pub fn add_invocation_message(
+            &mut self,
+            _end_time: SystemTime,
+            _exit_code: Option<i32>,
+            _custom_map: Option<&HashMap<String, String>>,
+        ) {
+        }
+
+        pub fn write_invocation_message(
+            &mut self,
+            _exit_code: Option<i32>,
+            _custom_map: Option<&HashMap<String, String>>,
+        ) {
+        }
+    }
+}
