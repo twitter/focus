0397fc826d71078488b753ea05c53a44cea7eb1a
David Bernadett <dbernadett@twitter.com>
2022-08-18 11:47:44 -0700

    Add arguments to the script in order to remove twitter specific code, record ouput of focus

--- scripts/performance-benchmark/run.sh
+++ scripts/performance-benchmark/run.sh
@@ -8,55 +8,62 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SCRIPT_NAME=$(basename "$0")
 SANDBOX_DIR="$SCRIPT_NAME.sandbox"
 
-# rm -rf "$SANDBOX_DIR"
+# Get Arguments
+if [[ -z "${FOCUS_PERF_REF_PREFIX}" ]]; then
+    echo "FOCUS_PERF_REF_PREFIX must be set"
+    exit
+fi
+if [[ -z "${FOCUS_REPO_DIR}" ]]; then
+    echo "FOCUS_REPO_DIR must be set"
+    exit
+fi
+
 mkdir -p trash
 mkdir -p "$SANDBOX_DIR"
 
 # remove sandbox in the background
-mv "$SANDBOX_DIR" trash/.
-nohup rm -rf trash/"$SANDBOX_DIR" &>/dev/null &
+RANDOM_NAME="$(dd if=/dev/urandom count=8 bs=1 | xxd -p)"
+mv "$SANDBOX_DIR" "trash/$RANDOM_NAME"
+nohup rm -rf trash/"$RANDOM_NAME" &>/dev/null &
 
 mkdir -p "$SANDBOX_DIR"
 
 FIXTURE_DIR="focus-performance.fixture"
 # Reset the remote
-git -C "$FIXTURE_DIR/source" branch -f master origin/dbernadett/focus-performance-0.1
-
-# Get a clean dense repo
-#cp -r -P "$FIXTURE_DIR/source-dense" "$SANDBOX_DIR/."
+git -C "$FIXTURE_DIR/source" branch -f master "origin/${FOCUS_PERF_REF_PREFIX}1"
 
 cd "$SANDBOX_DIR"
 
 # Bash function defined:
 focus.devb() {
-  ~/workspace/focus/target/debug/focus $@
+  "${FOCUS_REPO_DIR}/target/debug/focus" $@
 }
 
 # May need to specify some projects after focus new
 rm -rf source-focused
 echo "focus new:" > timings.txt
 date >> timings.txt
-time focus.devb new --dense-repo="../$FIXTURE_DIR/source-dense" source-focused
+focus.devb new --dense-repo="../$FIXTURE_DIR/source-dense" source-focused 2> >(tee focus.new.1)
 date >> timings.txt
 
 cd source-focused
 
 # Test sync after pull
-git -C "../../$FIXTURE_DIR/source" branch -f master origin/dbernadett/focus-performance-0.2 
+git -C "../../$FIXTURE_DIR/source" branch -f master "origin/${FOCUS_PERF_REF_PREFIX}2"
 git pull
-echo "focus sync to focus-performance-0.2:" >> ../timings.txt
+echo "focus sync to ${FOCUS_PERF_REF_PREFIX}2:" >> ../timings.txt
 # We may not need to call sync explicitly, if the git pull is working
 date >> ../timings.txt
-focus.devb sync
+focus.devb sync 2> >(tee ../focus.sync.1)
 date >> ../timings.txt
 
 # Test sync after pull
-git -C "../../$FIXTURE_DIR/source" branch -f master origin/dbernadett/focus-performance-0.3
+git -C "../../$FIXTURE_DIR/source" branch -f master "origin/${FOCUS_PERF_REF_PREFIX}3"
 git pull
-echo "focus sync to focus-performance-0.3:" >> ../timings.txt
+echo "focus sync to ${FOCUS_PERF_REF_PREFIX}3:" >> ../timings.txt
 # We may not need to call sync explicitly, if the git pull is working
 date >> ../timings.txt
-focus.devb sync
+focus.devb sync 2> >(tee ../focus.sync.2)
 date >> ../timings.txt
 
 
--- scripts/performance-benchmark/setup.sh
+++ scripts/performance-benchmark/setup.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-#Allow script to be run from anywhere
+# Allow script to be run from anywhere
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 CURRENT_DIR=$(realpath $(pwd))
+
+# Get Arguments
+if [ -z "${FOCUS_PERF_REMOTE}" ]; then
+    echo "FOCUS_PERF_REMOTE must be set"
+    exit
+fi
+if [[ -z "${FOCUS_PERF_REF_PREFIX}" ]]; then
+    echo "FOCUS_PERF_REF_PREFIX must be set"
+    exit
+fi
 
 # Setup clean fixtures
 SCRIPT_NAME=$(basename "$0")
@@ -14,11 +24,11 @@ cd "$FIXTURE_DIR"
 
 git init source --bare
 cd source
-git remote add origin https://git.twitter.biz/ro/source --no-tags
+git remote add origin "$FOCUS_PERF_REMOTE" --no-tags
 # Only make the fixture as deep as the first branch. 0.0 branch is about 100 days older than 0.1
-git ls-remote origin dbernadett/focus-performance-0.0 | awk '{print $1}' >> shallow
-git config remote.origin.fetch '+refs/heads/dbernadett/focus-performance-0.*:refs/remotes/origin/dbernadett/focus-performance-0.*'
+git ls-remote origin "${FOCUS_PERF_REF_PREFIX}0" | awk '{print $1}' >> shallow
+git config remote.origin.fetch "+refs/heads/${FOCUS_PERF_REF_PREFIX}*:refs/remotes/origin/${FOCUS_PERF_REF_PREFIX}*"
 git fetch
-git branch master origin/dbernadett/focus-performance-0.1 --no-track
+git branch master "origin/${FOCUS_PERF_REF_PREFIX}1" --no-track
 cd ..
 git clone file:///$CURRENT_DIR/$FIXTURE_DIR/source source-dense
