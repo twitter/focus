a8ae6b9811b0f9e2eb1b21ea587c1028b2808cfe
David Bernadett <dbernadett@twitter.com>
2022-08-18 15:54:45 -0700

    remove twttr as a default feature

--- scripts/mac-build.sh
+++ scripts/mac-build.sh
@@ -12,4 +12,4 @@ exec env \
      TARGET=x86_64-apple-darwin \
      MACOSX_DEPLOYMENT_TARGET=10.15 \
      RUSTFLAGS="-Awarnings" \
-     cargo build --release
+     cargo build --features twttr --release
