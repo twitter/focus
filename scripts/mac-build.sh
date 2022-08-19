7ae7b1ee799c47f75007a0db9350d99604fd9e4e
Kiran Paul <kdspaul@gmail.com>
2022-08-19 00:24:35 -0700

    Check that working tree is clean before building

--- scripts/mac-build.sh
+++ scripts/mac-build.sh
@@ -2,9 +2,13 @@
 # Copyright 2022 Twitter, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
+set -euo pipefail
 
-set -eu
-set -o pipefail
+# Run some basic checks to make sure that we are building off a clean state
+if git status --porcelain | grep -q . ; then
+    echo "Please clean your working tree  before building"
+    exit 1
+fi
 
 set -x
 
