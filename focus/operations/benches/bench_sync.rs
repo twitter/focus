0d55094b05a22f073aa7648782ae8d665d2ec43c
Waleed Khan <me@waleedkhan.name>
2022-09-07 14:19:58 -0700

    temp: create benchmark for sync

---
+++ focus/operations/benches/bench_sync.rs
@@ -0,0 +1,39 @@
+// Copyright 2022 Twitter, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::path::PathBuf;
+use std::sync::Arc;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use focus_util::app::App;
+
+pub fn bench_sync(c: &mut Criterion) {
+    let app = Arc::new(App::new_for_testing().unwrap());
+    let repo_path = std::env::var_os("REPO")
+        .map(PathBuf::from)
+        .expect("Must set env var REPO=/path/to/repo");
+
+    println!("Warming up with initial sync");
+    focus_operations::sync::run(
+        &repo_path,
+        focus_operations::sync::SyncMode::Normal,
+        app.clone(),
+    )
+    .unwrap();
+
+    let mut group = c.benchmark_group("noop_sync");
+    group.sample_size(20);
+    group.bench_function("focus_operations::sync::run", |b| {
+        b.iter(|| {
+            focus_operations::sync::run(
+                &repo_path,
+                focus_operations::sync::SyncMode::Normal,
+                app.clone(),
+            )
+            .unwrap()
+        })
+    });
+}
+
+criterion_group!(benches, bench_sync);
+criterion_main!(benches);
