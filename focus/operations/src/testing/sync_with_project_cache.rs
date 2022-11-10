8da64cd672f62a5513739ed388035bd41557a241
Wilhelm Bierbaum <wilhelm@twitter.com>
2022-11-10 13:10:23 -0800

    Add a (as of yet failing) test exercising project cache generation in the dense repo

--- focus/operations/src/testing/sync_with_project_cache.rs
+++ focus/operations/src/testing/sync_with_project_cache.rs
@@ -1,6 +1,8 @@
 // Copyright 2022 Twitter, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use std::path::Path;
+
 use anyhow::{bail, Context, Result};
 use focus_internals::model::repo::PROJECT_CACHE_ENDPOINT_CONFIG_KEY;
 use focus_testing::init_logging;
@@ -12,22 +14,39 @@ use crate::{
     testing::integration::RepoPairFixture,
 };
 
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+enum Location {
+    Sparse,
+    Dense,
+}
+
 struct Fixture {
     underlying: RepoPairFixture,
+    location: Location,
 }
 
 impl Fixture {
-    fn new() -> Result<Self> {
+    fn new(location: Location) -> Result<Self> {
         let underlying = RepoPairFixture::new()?;
         underlying.perform_clone()?;
-        Ok(Self { underlying })
+        Ok(Self {
+            underlying,
+            location,
+        })
+    }
+
+    fn repo_path(&self) -> &Path {
+        match &self.location {
+            Location::Sparse => self.underlying.sparse_repo_path.as_path(),
+            Location::Dense => self.underlying.dense_repo_path.as_path(),
+        }
     }
 
     fn configure_endpoint(&self) -> Result<()> {
         let path = self.underlying.dir.path().join("project_cache_db");
         std::fs::create_dir_all(&path)?;
         git_helper::write_config(
-            &self.underlying.sparse_repo_path,
+            self.repo_path(),
             PROJECT_CACHE_ENDPOINT_CONFIG_KEY,
             format!("file://{}", path.display()).as_str(),
             self.underlying.app.clone(),
@@ -41,7 +60,7 @@ impl Fixture {
         for shard_index in 0..shard_count {
             let exit_code = project_cache::push(
                 app.clone(),
-                &self.underlying.sparse_repo_path,
+                self.repo_path(),
                 String::from("HEAD"),
                 shard_index,
                 shard_count,
@@ -70,7 +89,7 @@ impl Fixture {
 fn generation() -> Result<()> {
     init_logging();
 
-    let fixture = Fixture::new()?;
+    let fixture = Fixture::new(Location::Sparse)?;
     fixture.configure_endpoint()?;
     fixture.generate_content(2)?;
 
@@ -81,7 +100,7 @@ fn generation() -> Result<()> {
 fn project_cache_falls_back_with_non_project_targets_selected() -> Result<()> {
     init_logging();
 
-    let fixture = Fixture::new()?;
+    let fixture = Fixture::new(Location::Sparse)?;
     let app = fixture.underlying.app.clone();
     fixture.configure_endpoint()?;
 
@@ -119,7 +138,7 @@ fn project_cache_falls_back_with_non_project_targets_selected() -> Result<()> {
 fn project_cache_answers_with_only_projects_selected() -> Result<()> {
     init_logging();
 
-    let fixture = Fixture::new()?;
+    let fixture = Fixture::new(Location::Sparse)?;
     let app = fixture.underlying.app.clone();
     fixture.configure_endpoint()?;
     fixture.generate_content(2)?;
@@ -146,11 +165,10 @@ fn project_cache_answers_with_only_projects_selected() -> Result<()> {
     Ok(())
 }
 
-#[test]
-fn project_cache_generates_all_projects() -> Result<()> {
+fn project_cache_generates_all_projects_internal(location: Location) -> Result<()> {
     init_logging();
 
-    let fixture = Fixture::new()?;
+    let fixture = Fixture::new(location)?;
     let app = fixture.underlying.app.clone();
     fixture.configure_endpoint()?;
     fixture.generate_content(10)?;
@@ -183,4 +201,15 @@ fn project_cache_generates_all_projects() -> Result<()> {
     assert_eq!(result.mechanism, SyncMechanism::ProjectCache);
 
     Ok(())
+}
+
+#[test]
+fn project_cache_generates_all_projects_with_sparse_repo() -> Result<()> {
+    project_cache_generates_all_projects_internal(Location::Sparse)
+}
+
+#[ignore = "Needs underlying implementation"]
+#[test]
+fn project_cache_generates_all_projects_with_dense_repo() -> Result<()> {
+    project_cache_generates_all_projects_internal(Location::Dense)
 }
