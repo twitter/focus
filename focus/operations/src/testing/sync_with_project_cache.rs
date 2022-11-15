05e459a423214ccca4b22378c83bb845151d0559
Wilhelm Bierbaum <wilhelm@twitter.com>
2022-11-15 11:42:52 -0800

    Project Cache: generation in dense repos and tests working

--- focus/operations/src/testing/sync_with_project_cache.rs
+++ focus/operations/src/testing/sync_with_project_cache.rs
@@ -36,17 +36,21 @@ impl Fixture {
     }
 
     fn repo_path(&self) -> &Path {
-        match &self.location {
+        self.repo_path_for_location(self.location)
+    }
+
+    fn repo_path_for_location(&self, location: Location) -> &Path {
+        match location {
             Location::Sparse => self.underlying.sparse_repo_path.as_path(),
             Location::Dense => self.underlying.dense_repo_path.as_path(),
         }
     }
 
-    fn configure_endpoint(&self) -> Result<()> {
+    fn configure_endpoint(&self, location: Location) -> Result<()> {
         let path = self.underlying.dir.path().join("project_cache_db");
         std::fs::create_dir_all(&path)?;
         git_helper::write_config(
-            self.repo_path(),
+            self.repo_path_for_location(location),
             PROJECT_CACHE_ENDPOINT_CONFIG_KEY,
             format!("file://{}", path.display()).as_str(),
             self.underlying.app.clone(),
@@ -90,7 +94,7 @@ fn generation() -> Result<()> {
     init_logging();
 
     let fixture = Fixture::new(Location::Sparse)?;
-    fixture.configure_endpoint()?;
+    fixture.configure_endpoint(Location::Sparse)?;
     fixture.generate_content(2)?;
 
     Ok(())
@@ -102,7 +106,7 @@ fn project_cache_falls_back_with_non_project_targets_selected() -> Result<()> {
 
     let fixture = Fixture::new(Location::Sparse)?;
     let app = fixture.underlying.app.clone();
-    fixture.configure_endpoint()?;
+    fixture.configure_endpoint(Location::Sparse)?;
 
     // Add a project and a directory target to the selection
     crate::selection::add(
@@ -140,7 +144,7 @@ fn project_cache_answers_with_only_projects_selected() -> Result<()> {
 
     let fixture = Fixture::new(Location::Sparse)?;
     let app = fixture.underlying.app.clone();
-    fixture.configure_endpoint()?;
+    fixture.configure_endpoint(Location::Sparse)?;
     fixture.generate_content(2)?;
 
     // Add a project and a directory target to the selection
@@ -170,8 +174,14 @@ fn project_cache_generates_all_projects_internal(location: Location) -> Result<(
 
     let fixture = Fixture::new(location)?;
     let app = fixture.underlying.app.clone();
-    fixture.configure_endpoint()?;
+    if location == Location::Dense {
+        // Also configure sparse since we will query there.
+        fixture.configure_endpoint(Location::Sparse)?;
+    }
+    fixture.configure_endpoint(location)?;
     fixture.generate_content(10)?;
+
+    tracing::debug!(repo = ?fixture.repo_path());
 
     let selection_manager = fixture.underlying.sparse_repo()?.selection_manager()?;
     let project_names: Vec<String> = selection_manager
